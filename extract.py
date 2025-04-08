b7be508ed4e4955197184183f7b24cd1635b0f33
jamorrison <jamorrison@users.noreply.github.com>
2025-04-07 22:35:50 -0400

    finish extract code

--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 import requests
 import zipfile
 import pandas as pd
+import numpy as np
+import os
 
 # Code is based on this StackOverflow post:
 #     https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
@@ -27,14 +29,32 @@ def download_file(url, fname):
 
     return None
 
-# TODO: Add documentation
 def csv_to_df(data):
+    """Turn a CSV file into a DataFrame.
+
+    Inputs -
+        data - file contents as a string
+    Returns -
+        pandas.DataFrame
+    """
     lines = [row.split(',') for row in data.split('\n')]
 
-    # TODO: Add NA from blank string
-    return pd.DataFrame(lines)
+    tmp = pd.DataFrame(lines)
+    tmp.columns = tmp.iloc[0]
+    tmp = tmp.drop(tmp.index[0])
+
+    # This returns a FutureWarning that should be addressed, but will be ignored for now
+    return tmp.replace(r'^\s*$', np.nan, regex=True)
 
 def get_data(zh, sub_fname):
+    """Retrieve data from within a zip file
+
+    Inputs -
+        zh        - zip file handle
+        sub_fname - compressed file name within zip file
+    Returns -
+        pandas.DataFrame
+    """
     with zh.open(sub_fname) as zf:
         data = zf.read().decode('utf-8').strip()
         df = csv_to_df(data)
@@ -42,6 +62,13 @@ def get_data(zh, sub_fname):
     return df
 
 def retrieve_csv_files(fname):
+    """Get the specific data that I want to parse
+
+    Inputs -
+        fname - file name of zip file
+    Returns -
+        tuple, (pandas.DataFrame, pandas.DataFrame, pandas.DataFrame)
+    """
     with zipfile.ZipFile(fname) as zh:
         df1 = get_data(zh, 'Teams.csv')
         df2 = get_data(zh, 'TeamSplits.csv')
@@ -50,19 +77,38 @@ def retrieve_csv_files(fname):
     return (df1, df2, df3)
 
 def extract():
+    """Pull data from Kaggle and extract to joined DataFrame
+
+    Inputs -
+        None
+    Returns -
+        pandas.DataFrame
+    """
     # Remote URL and local filename
     kaggle_file = 'https://www.kaggle.com/api/v1/datasets/download/open-source-sports/professional-hockey-database'
     local_file = 'professional_hockey_database.zip'
 
+    # Download data
     try:
         download_file(kaggle_file, local_file)
     except requests.HTTPError:
         print(f'Unable to download file from: {kaggle_file}')
         exit(1)
 
-    # TODO: Need to do joins on data
+    # Extract data
     df1, df2, df3 = retrieve_csv_files(local_file)
 
-    # TODO: Delete downloaded local file once all data has been extracted
+    # Merge data
+    df = pd.merge(df1, df2, on=['year', 'lgID', 'tmID'], how='left')
+    df = pd.merge(df3, df, on=['year', 'lgID', 'tmID'], how='left', suffixes=['_TvT', '_split'])
 
-extract()
+    try:
+        os.remove(local_file)
+    except FileNotFoundError:
+        print('Attempted to delete {local_file}, but could not find it')
+
+    return df
+
+if __name__ == '__main__':
+    df = extract()
+    print(df)
